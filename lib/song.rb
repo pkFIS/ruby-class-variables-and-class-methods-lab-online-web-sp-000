1801b0f13100e882c6c8a5daa81e35236f4bfc7b
Paul K <pkfis@outlook.com>
2019-05-04 00:13:34 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @@count += 1
+
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  def genre_count
+    @@genres
+  end
+
+  def artist_count
+    @@artists
+  end
+
+
+
+end
